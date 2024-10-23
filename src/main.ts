1e4f233bfa97f7c4b14d581a9ff6fd1ac45838ca
saminame <sesteqla@ucsc.edu>
2024-10-22 20:26:26 -0700

    Step 1: Initial non-interactive UI layout

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,6 @@ const APP_NAME = "Sims Deluxe";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+// Step 1: Initial non-interactive UI layout
+app.innerHTML = app.innerHTML = `<h1>${APP_NAME}</h1><canvas id="gameCanvas" width="256" height="256"></canvas>`;
+
